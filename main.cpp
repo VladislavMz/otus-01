8c6631358eb823c55d710cf30865e0d379382af1
Vladislav Mz <vlad.mazurenko.95@gmail.com>
2021-04-12 17:02:56 +0700

    cmake_2

--- main.cpp
+++ main.cpp
@@ -1,11 +1,16 @@
+#include <iostream>
+
 #include "lib.h"
 
-#include <spdlog/spdlog.h>
+// #include <spdlog/spdlog.h>
 
 int main()
 {
-    auto logger = spdlog::stdout_logger_mt("console");
-    logger->info("version {} was started", version());
+    // auto logger = spdlog::stdout_logger_mt("console");
+    // logger->info("version {} was started", version());
+
+    std::cout << "build " << version() << std::endl;
+    std::cout << "Hello, World!" << std::endl;
 
     return 0;
 }
